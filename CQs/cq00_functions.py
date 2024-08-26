f9a2e51cc1379931e7e9d907e0d53b3da047af5c
jfari628 <jfrai@ad.unc.edu>
2024-08-26 18:00:39 -0400

    First CQ!

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,12 @@
+"""First Challenge Question!"""
+
+__author__ = "730554105"
+
+
+def mimic(message: str) -> str:
+    """Return this message back to me."""
+    return message
+
+
+if __name__ == "__main__":
+    print(mimic(message=input("What is your message?")))
