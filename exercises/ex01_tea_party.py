40549000a4f7eaeece788a74e62fbbb752ad0c0a
jfari628 <jfrai@ad.unc.edu>
2024-08-28 09:46:13 -0400

    Step 1 of EX01 and part of Step 2

---
+++ exercises/ex01_tea_party.py
@@ -0,0 +1,21 @@
+"""A program to help plan a tea party"""
+
+__author__: str = "730554105"
+
+
+def tea_bags(people: int) -> int:  # Defining the tea_bags function
+    """Calculate the number of tea bags based on the number of people"""
+    return people * 2
+
+
+# Need to multiply the number of people times 2
+# Multiply the return value of the tea_bags function by 2
+
+
+def treats(people: int) -> int:
+    """Calculate the number of treats based on the number of people"""
+    return people
+
+
+# Account for relationship between number of teas drank and number of treats to be eaten
+# Need to use a keyword argument but idk what that is yet so come back later
